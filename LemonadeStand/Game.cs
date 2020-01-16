51635486800cbc24447efbb035fa2d248d53f4e9
unknown <svclennan@icloud.com>
2020-01-16 11:47:48 -0600

    Created the consturctor and Property. Created the Game constructor, making a player and setting the number of days.

--- LemonadeStand/Game.cs
+++ LemonadeStand/Game.cs
@@ -11,5 +11,37 @@ namespace LemonadeStand
         Player player;
         List<Day> days;
         int currentDay;
+
+        public Game()
+        {
+            player = new Player(GetName());
+            days = SetDays();
+            currentDay = 0;
+        }
+        public string GetName()
+        {
+            Console.WriteLine("What is your name?");
+            return Console.ReadLine();
+        }
+        public List<Day> SetDays()
+        {
+            int numOfDays;
+            List<Day> dayList = new List<Day>();
+            Console.WriteLine("How many days would you like to play?");
+            try
+            {
+                numOfDays = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("That is not a number of days.");
+                return SetDays();
+            }
+            for (int i = 0; i < numOfDays; i++)
+            {
+                dayList.Add(new Day());
+            }
+            return dayList;
+        }
     }
 }
